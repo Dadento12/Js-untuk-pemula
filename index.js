1b800b3ea120967286ed0ae3675a50d248870051
Dadento12 <dandankahayan86@gmail.com>
2025-02-09 00:45:35 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Deklarasi variabel menggunakan var
+// `var` adalah cara lama untuk mendeklarasikan variabel. Variabel yang dideklarasikan dengan `var` dapat diubah nilainya dan memiliki cakupan fungsi atau global.
+var nama = "Alice";
+console.log(nama);
+
+// Deklarasi variabel menggunakan let
+// `let` digunakan untuk mendeklarasikan variabel dengan cakupan scope. Variabel yang dideklarasikan dengan `let` dapat diubah nilainya dan lebih disukai daripada `var` karena memiliki cakupan yang lebih ketat.
+let umur = 25;
+console.log(umur);
+
+// Deklarasi variabel menggunakan const
+// `const` digunakan untuk mendeklarasikan variabel konstan yang nilainya tidak dapat diubah setelah dideklarasikan. `const` juga memiliki cakupan blok atau scope.
+const negara = "Indonesia";
+console.log(negara);
